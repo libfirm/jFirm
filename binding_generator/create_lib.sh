7ea960257200ff9a25f4104197e2b9ee0e555ce7
Matthias Braun <matze@braunis.de>
2008-12-15 10:20:07 +0000

    - put lib into lib directory
    git-svn-id: file:///ben/local/SVN/cb/trunk/JFirm@61 57d115a9-7d6f-487c-9cec-30711b30cee7

--- binding_generator/create_lib.sh
+++ binding_generator/create_lib.sh
@@ -2,7 +2,7 @@
 
 . config
 
-GOAL="../libfirm.so"
+GOAL="../lib/libfirm.so"
 echo -e "#include <libfirm/firm.h>\nvoid __dummy__(void) { ir_get_version_major(); }" > /tmp/empty.c || exit $?
 CMD="gcc -shared /tmp/empty.c -o ${GOAL} -I${FIRM_INC} ${FIRM_LFLAGS}"
 echo $CMD
