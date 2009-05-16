a9cbac2db787c78ef1f105e19cc80cd4b64a9a14
Jürgen Graf <grafj@ipd.info.uni-karlsruhe.de>
2009-05-16 15:22:59 +0000

    firm with osx callback for assertions sourcecode attached for jna new jna
    git-svn-id: file:///ben/local/SVN/cb/trunk/JFirm@132 57d115a9-7d6f-487c-9cec-30711b30cee7

--- src/firm/Firm.java
+++ src/firm/Firm.java
@@ -1,16 +1,28 @@
 package firm;
 
+import com.sun.jna.Callback;
+import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.Pointer;
 
 import firm.bindings.Bindings;
 import firm.bindings.binding_firm_common;
 import firm.bindings.binding_irflag;
-import firm.bindings.binding_libc;
 
 public final class Firm {
 	private static final binding_firm_common binding = Bindings.getFirmCommonBinding();
 	private static final binding_irflag binding_flag = (binding_irflag) Native.loadLibrary("firm", binding_irflag.class);
+	
+	public interface binding_callback extends Library {
+
+		interface FirmCallback extends Callback {
+			void callback(String expr, String file, int line);
+		}
+	
+		void firm_set_assert_callback(FirmCallback handler);
+	}	
+
+	private static final binding_callback binding_cb = (binding_callback) Native.loadLibrary("firm", binding_callback.class);
 	
 	public static int getMajorVersion() {
 		return binding.ir_get_version_major();
@@ -27,17 +39,19 @@ public final class Firm {
 	public static String getRevision() {
 		return binding.ir_get_version_revision();
 	}
-	
-	public static binding_libc.SigHandler handler = new binding_libc.SigHandler() {
+		
+	private static final binding_callback.FirmCallback handler = new binding_callback.FirmCallback() {
 		@Override
-		public void callback(int arg) {
-			throw new RuntimeException("Prog Aborted");
+		public void callback(String expr, String file, int line) {
+			throw new FirmAssertionException(expr, file, line);
+//			System.err.printf("Failed assert('%1s') in %2s:%3d", expr, file, line);
 		}			
 	}; 
 	
 	public static void init() {
 		/* hack to catch asserts... */
-		Backend.binding_c.signal(/*SIGABRT*/ 6, handler);
+//		Backend.binding_c.signal(/*SIGABRT*/ 6, handler);
+		binding_cb.firm_set_assert_callback(handler);
 		
 		binding.ir_init(Pointer.NULL);
 		
---
+++ src/firm/FirmAssertionException.java
@@ -0,0 +1,21 @@
+package firm;
+
+public class FirmAssertionException extends RuntimeException {
+	
+	private final int lineNr;
+	private final String file;
+	private final String expr;
+
+	public FirmAssertionException(String expr, String file, int lineNr) {
+		super();
+		this.expr = expr;
+		this.file = file;
+		this.lineNr = lineNr;
+	}
+
+	@Override
+	public String getMessage() {
+		return "Failed assert('" + expr + "') in " + file + ":" + lineNr;
+	}
+	
+}
