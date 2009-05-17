b3213389c95a0048540aeeeb83925b42c6a4cd50
Jürgen Graf <grafj@ipd.info.uni-karlsruhe.de>
2009-05-17 12:14:08 +0000

    make callback optional
    git-svn-id: file:///ben/local/SVN/cb/trunk/JFirm@133 57d115a9-7d6f-487c-9cec-30711b30cee7

--- src/firm/Firm.java
+++ src/firm/Firm.java
@@ -10,6 +10,11 @@ import firm.bindings.binding_firm_common;
 import firm.bindings.binding_irflag;
 
 public final class Firm {
+	/**
+	 * See README.OSX for details of the callback stuff
+	 */
+	public static final boolean USE_FIRM_ASSERT_CALLBACK = true;
+	
 	private static final binding_firm_common binding = Bindings.getFirmCommonBinding();
 	private static final binding_irflag binding_flag = (binding_irflag) Native.loadLibrary("firm", binding_irflag.class);
 	
@@ -22,8 +27,6 @@ public final class Firm {
 		void firm_set_assert_callback(FirmCallback handler);
 	}	
 
-	private static final binding_callback binding_cb = (binding_callback) Native.loadLibrary("firm", binding_callback.class);
-	
 	public static int getMajorVersion() {
 		return binding.ir_get_version_major();
 	}
@@ -40,18 +43,24 @@ public final class Firm {
 		return binding.ir_get_version_revision();
 	}
 		
-	private static final binding_callback.FirmCallback handler = new binding_callback.FirmCallback() {
-		@Override
-		public void callback(String expr, String file, int line) {
-			throw new FirmAssertionException(expr, file, line);
-//			System.err.printf("Failed assert('%1s') in %2s:%3d", expr, file, line);
-		}			
-	}; 
+	private static final binding_callback binding_cb = (USE_FIRM_ASSERT_CALLBACK ?
+		(binding_callback) Native.loadLibrary("firm", binding_callback.class) : null);
+	
+	private static final binding_callback.FirmCallback handler = (USE_FIRM_ASSERT_CALLBACK ?
+		new binding_callback.FirmCallback() {
+			@Override
+			public void callback(String expr, String file, int line) {
+				throw new FirmAssertionException(expr, file, line);
+//				System.err.printf("Failed assert('%1s') in %2s:%3d", expr, file, line);
+			}			
+		} : null); 
 	
 	public static void init() {
 		/* hack to catch asserts... */
 //		Backend.binding_c.signal(/*SIGABRT*/ 6, handler);
-		binding_cb.firm_set_assert_callback(handler);
+		if (USE_FIRM_ASSERT_CALLBACK) {
+			binding_cb.firm_set_assert_callback(handler);
+		}
 		
 		binding.ir_init(Pointer.NULL);
 		
