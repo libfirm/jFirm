eb8a4b6d88dbfeb878b0490f9a2e96662deaba25
Matthias Braun <matze@braunis.de>
2010-11-22 14:49:30 +0100

    assert that backend is only used once

--- src/firm/Backend.java
+++ src/firm/Backend.java
@@ -8,10 +8,18 @@ import firm.bindings.binding_be;
 import firm.bindings.binding_iroptimize;
 import firm.bindings.binding_libc;
 
-public class Backend {
+public final class Backend {
+	private static int runs = 0;
+
+	private Backend() {
+	}
 
 	public static void createAssembler(String outputFileName,
 			String compilationUnitName) throws IOException {
+		/* running the backend twice is currently not supported */
+		assert runs == 0;
+		runs++;
+
 		Pointer file = binding_libc.fopen(outputFileName, "w");
 		if (file == null) {
 			throw new IOException("Couldn't open output file (write access): "
