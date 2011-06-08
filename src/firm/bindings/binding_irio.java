0186cfbe89b39e4c51d0a31f53aee0d60d58bb66
Matthias Braun <matze@braunis.de>
2011-06-08 14:47:01 +0200

    add bindings for irio

---
+++ src/firm/bindings/binding_irio.java
@@ -0,0 +1,424 @@
+package firm.bindings;
+
+/* WARNING: Automatically generated file */
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+
+public class binding_irio {
+	static {
+		Native.register("firm");
+	}
+
+	public static enum __codecvt_result {
+		__codecvt_ok(),
+		__codecvt_partial(),
+		__codecvt_error(),
+		__codecvt_noconv();
+		public final int val;
+
+		private static class C {
+			static int next_val;
+		}
+
+		__codecvt_result(int val) {
+			this.val = val;
+			C.next_val = val + 1;
+		}
+
+		__codecvt_result() {
+			this.val = C.next_val++;
+		}
+
+		public static __codecvt_result getEnum(int val) {
+			for (__codecvt_result entry : values()) {
+				if (val == entry.val)
+					return entry;
+			}
+			return null;
+		}
+	}
+
+	public static enum ir_relation {
+		ir_relation_false(0),
+		ir_relation_equal((1 << 0)),
+		ir_relation_less((1 << 1)),
+		ir_relation_greater((1 << 2)),
+		ir_relation_unordered((1 << 3)),
+		ir_relation_less_equal((ir_relation.ir_relation_equal.val | ir_relation.ir_relation_less.val)),
+		ir_relation_greater_equal((ir_relation.ir_relation_equal.val | ir_relation.ir_relation_greater.val)),
+		ir_relation_less_greater((ir_relation.ir_relation_less.val | ir_relation.ir_relation_greater.val)),
+		ir_relation_less_equal_greater(((ir_relation.ir_relation_equal.val | ir_relation.ir_relation_less.val) | ir_relation.ir_relation_greater.val)),
+		ir_relation_unordered_equal((ir_relation.ir_relation_unordered.val | ir_relation.ir_relation_equal.val)),
+		ir_relation_unordered_less((ir_relation.ir_relation_unordered.val | ir_relation.ir_relation_less.val)),
+		ir_relation_unordered_less_equal(((ir_relation.ir_relation_unordered.val | ir_relation.ir_relation_less.val) | ir_relation.ir_relation_equal.val)),
+		ir_relation_unordered_greater((ir_relation.ir_relation_unordered.val | ir_relation.ir_relation_greater.val)),
+		ir_relation_unordered_greater_equal(((ir_relation.ir_relation_unordered.val | ir_relation.ir_relation_greater.val) | ir_relation.ir_relation_equal.val)),
+		ir_relation_unordered_less_greater(((ir_relation.ir_relation_unordered.val | ir_relation.ir_relation_less.val) | ir_relation.ir_relation_greater.val)),
+		ir_relation_true((((ir_relation.ir_relation_equal.val | ir_relation.ir_relation_less.val) | ir_relation.ir_relation_greater.val) | ir_relation.ir_relation_unordered.val));
+		public final int val;
+
+		private static class C {
+			static int next_val;
+		}
+
+		ir_relation(int val) {
+			this.val = val;
+			C.next_val = val + 1;
+		}
+
+		ir_relation() {
+			this.val = C.next_val++;
+		}
+
+		public static ir_relation getEnum(int val) {
+			for (ir_relation entry : values()) {
+				if (val == entry.val)
+					return entry;
+			}
+			return null;
+		}
+	}
+
+	public static enum ir_cons_flags {
+		cons_none(0),
+		cons_volatile((1 << 0)),
+		cons_unaligned((1 << 1)),
+		cons_floats((1 << 2));
+		public final int val;
+
+		private static class C {
+			static int next_val;
+		}
+
+		ir_cons_flags(int val) {
+			this.val = val;
+			C.next_val = val + 1;
+		}
+
+		ir_cons_flags() {
+			this.val = C.next_val++;
+		}
+
+		public static ir_cons_flags getEnum(int val) {
+			for (ir_cons_flags entry : values()) {
+				if (val == entry.val)
+					return entry;
+			}
+			return null;
+		}
+	}
+
+	public static enum op_pin_state {
+		op_pin_state_floats(0),
+		op_pin_state_pinned(1),
+		op_pin_state_exc_pinned(),
+		op_pin_state_mem_pinned();
+		public final int val;
+
+		private static class C {
+			static int next_val;
+		}
+
+		op_pin_state(int val) {
+			this.val = val;
+			C.next_val = val + 1;
+		}
+
+		op_pin_state() {
+			this.val = C.next_val++;
+		}
+
+		public static op_pin_state getEnum(int val) {
+			for (op_pin_state entry : values()) {
+				if (val == entry.val)
+					return entry;
+			}
+			return null;
+		}
+	}
+
+	public static enum cond_jmp_predicate {
+		COND_JMP_PRED_NONE(),
+		COND_JMP_PRED_TRUE(),
+		COND_JMP_PRED_FALSE();
+		public final int val;
+
+		private static class C {
+			static int next_val;
+		}
+
+		cond_jmp_predicate(int val) {
+			this.val = val;
+			C.next_val = val + 1;
+		}
+
+		cond_jmp_predicate() {
+			this.val = C.next_val++;
+		}
+
+		public static cond_jmp_predicate getEnum(int val) {
+			for (cond_jmp_predicate entry : values()) {
+				if (val == entry.val)
+					return entry;
+			}
+			return null;
+		}
+	}
+
+	public static enum mtp_additional_properties {
+		mtp_no_property(0x00000000),
+		mtp_property_const(0x00000001),
+		mtp_property_pure(0x00000002),
+		mtp_property_noreturn(0x00000004),
+		mtp_property_nothrow(0x00000008),
+		mtp_property_naked(0x00000010),
+		mtp_property_malloc(0x00000020),
+		mtp_property_returns_twice(0x00000040),
+		mtp_property_intrinsic(0x00000080),
+		mtp_property_runtime(0x00000100),
+		mtp_property_private(0x00000200),
+		mtp_property_has_loop(0x00000400),
+		mtp_property_inherited((1 << 31));
+		public final int val;
+
+		private static class C {
+			static int next_val;
+		}
+
+		mtp_additional_properties(int val) {
+			this.val = val;
+			C.next_val = val + 1;
+		}
+
+		mtp_additional_properties() {
+			this.val = C.next_val++;
+		}
+
+		public static mtp_additional_properties getEnum(int val) {
+			for (mtp_additional_properties entry : values()) {
+				if (val == entry.val)
+					return entry;
+			}
+			return null;
+		}
+	}
+
+	public static enum symconst_kind {
+		symconst_type_tag(),
+		symconst_type_size(),
+		symconst_type_align(),
+		symconst_addr_ent(),
+		symconst_ofs_ent(),
+		symconst_enum_const();
+		public final int val;
+
+		private static class C {
+			static int next_val;
+		}
+
+		symconst_kind(int val) {
+			this.val = val;
+			C.next_val = val + 1;
+		}
+
+		symconst_kind() {
+			this.val = C.next_val++;
+		}
+
+		public static symconst_kind getEnum(int val) {
+			for (symconst_kind entry : values()) {
+				if (val == entry.val)
+					return entry;
+			}
+			return null;
+		}
+	}
+
+	public static enum ir_where_alloc {
+		stack_alloc(),
+		heap_alloc();
+		public final int val;
+
+		private static class C {
+			static int next_val;
+		}
+
+		ir_where_alloc(int val) {
+			this.val = val;
+			C.next_val = val + 1;
+		}
+
+		ir_where_alloc() {
+			this.val = C.next_val++;
+		}
+
+		public static ir_where_alloc getEnum(int val) {
+			for (ir_where_alloc entry : values()) {
+				if (val == entry.val)
+					return entry;
+			}
+			return null;
+		}
+	}
+
+	public static enum ir_builtin_kind {
+		ir_bk_trap(),
+		ir_bk_debugbreak(),
+		ir_bk_return_address(),
+		ir_bk_frame_address(),
+		ir_bk_prefetch(),
+		ir_bk_ffs(),
+		ir_bk_clz(),
+		ir_bk_ctz(),
+		ir_bk_popcount(),
+		ir_bk_parity(),
+		ir_bk_bswap(),
+		ir_bk_inport(),
+		ir_bk_outport(),
+		ir_bk_inner_trampoline();
+		public final int val;
+
+		private static class C {
+			static int next_val;
+		}
+
+		ir_builtin_kind(int val) {
+			this.val = val;
+			C.next_val = val + 1;
+		}
+
+		ir_builtin_kind() {
+			this.val = C.next_val++;
+		}
+
+		public static ir_builtin_kind getEnum(int val) {
+			for (ir_builtin_kind entry : values()) {
+				if (val == entry.val)
+					return entry;
+			}
+			return null;
+		}
+	}
+
+	public static enum pn_generic {
+		pn_Generic_M(0),
+		pn_Generic_X_regular(1),
+		pn_Generic_X_except(2),
+		pn_Generic_other(3);
+		public final int val;
+
+		private static class C {
+			static int next_val;
+		}
+
+		pn_generic(int val) {
+			this.val = val;
+			C.next_val = val + 1;
+		}
+
+		pn_generic() {
+			this.val = C.next_val++;
+		}
+
+		public static pn_generic getEnum(int val) {
+			for (pn_generic entry : values()) {
+				if (val == entry.val)
+					return entry;
+			}
+			return null;
+		}
+	}
+
+	public static enum ir_value_classify_sign {
+		value_classified_unknown(0),
+		value_classified_positive(1),
+		value_classified_negative(-1);
+		public final int val;
+
+		private static class C {
+			static int next_val;
+		}
+
+		ir_value_classify_sign(int val) {
+			this.val = val;
+			C.next_val = val + 1;
+		}
+
+		ir_value_classify_sign() {
+			this.val = C.next_val++;
+		}
+
+		public static ir_value_classify_sign getEnum(int val) {
+			for (ir_value_classify_sign entry : values()) {
+				if (val == entry.val)
+					return entry;
+			}
+			return null;
+		}
+	}
+
+	public static enum ir_volatility {
+		volatility_non_volatile(),
+		volatility_is_volatile();
+		public final int val;
+
+		private static class C {
+			static int next_val;
+		}
+
+		ir_volatility(int val) {
+			this.val = val;
+			C.next_val = val + 1;
+		}
+
+		ir_volatility() {
+			this.val = C.next_val++;
+		}
+
+		public static ir_volatility getEnum(int val) {
+			for (ir_volatility entry : values()) {
+				if (val == entry.val)
+					return entry;
+			}
+			return null;
+		}
+	}
+
+	public static enum ir_align {
+		align_is_aligned(0),
+		align_non_aligned();
+		public final int val;
+
+		private static class C {
+			static int next_val;
+		}
+
+		ir_align(int val) {
+			this.val = val;
+			C.next_val = val + 1;
+		}
+
+		ir_align() {
+			this.val = C.next_val++;
+		}
+
+		public static ir_align getEnum(int val) {
+			for (ir_align entry : values()) {
+				if (val == entry.val)
+					return entry;
+			}
+			return null;
+		}
+	}
+
+
+	public static native void ir_export(String filename);
+
+	public static native void ir_export_file(Pointer output, String outputname);
+
+	public static native void ir_export_irg(Pointer irg, Pointer output, String outputname);
+
+	public static native void ir_import(String filename);
+
+	public static native void ir_import_file(Pointer input, String inputname);
+}
