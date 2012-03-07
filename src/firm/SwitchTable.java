b16099ee84ef6e32b1a6f0ef27cde7b3c9f44346
Matthias Braun <matthias.braun@kit.edu>
2012-03-07 16:25:49 +0100

    complete SwitchTable class

--- src/firm/SwitchTable.java
+++ src/firm/SwitchTable.java
@@ -14,4 +14,30 @@ public class SwitchTable extends JNAWrapper {
 	public SwitchTable(Graph irg, int numCases) {
 		this(binding_irnode.ir_new_switch_table(irg.ptr, new NativeLong(numCases)));
 	}
+
+	public final void setEntry(int entry, TargetValue min, TargetValue max, int pn) {
+		binding_irnode.ir_switch_table_set(ptr, new NativeLong(entry), min.ptr, max.ptr, new NativeLong(pn));
+	}
+
+	public final void setEntry(int entry, int min, int max, int pn) {
+		TargetValue minTV = new TargetValue(min, Mode.getIs());
+		TargetValue maxTV = new TargetValue(max, Mode.getIs());
+		setEntry(entry, minTV, maxTV, pn);
+	}
+
+	public final int getNEntries() {
+		return binding_irnode.ir_switch_table_get_n_entries(ptr).intValue();
+	}
+
+	public final TargetValue getMin(int entry) {
+		return new TargetValue(binding_irnode.ir_switch_table_get_min(ptr, new NativeLong(entry)));
+	}
+
+	public final TargetValue getMax(int entry) {
+		return new TargetValue(binding_irnode.ir_switch_table_get_max(ptr, new NativeLong(entry)));
+	}
+
+	public final int getPn(int entry) {
+		return binding_irnode.ir_switch_table_get_pn(ptr, new NativeLong(entry)).intValue();
+	}
 }
